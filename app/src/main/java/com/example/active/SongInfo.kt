6cf5910137a3d71fbfa50b9f6d0c4c448eab6bb5
alexandrefront <alexandre.front@tutamail.com>
2025-07-22 15:40:19 +0200

    empty

--- app/src/main/java/com/example/active/SongInfo.kt
+++
@@ -1,6 +0,0 @@
-package com.example.active
-
-data class SongInfo(
-    val title: String,
-    val artist: String
-)
--- app/src/main/java/com/example/active/models/StreamMetadata.kt
+++
@@ -1,8 +0,0 @@
-package com.example.active.models
-
-data class StreamMetadata(
-    val artist: String,
-    val title: String,
-    val thumb_url: String?,
-    val date_time: String?
-)
